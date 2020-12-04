f53c87faee85df8a9fa1d314c6334cec56a9c172
Daniel Berry <deb1g19@soton.ac.uk>
2020-12-04 12:46:49 +0000

    Implemented statusbarcolor control

--- ui/todo/lib/main.dart
+++ ui/todo/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo/constants.dart';
 import 'package:todo/screens/loading_screen.dart';
 import 'models/tasks_model.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(
@@ -12,28 +14,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //Status bar color is usually determined by scaffold, but scaffold color is not the same as the bg at the top of the screen, so this looks more natural
+    FlutterStatusbarcolor.setStatusBarColor(Colors.blue);
     return MaterialApp(
         title: 'Todo',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
             primarySwatch: Colors.blue,
             visualDensity: VisualDensity.adaptivePlatformDensity,
             textTheme: TextTheme(
                 bodyText1: TextStyle(
                     color: Colors.black,
-                    fontFamily: "Inter",
                     fontSize: 18,
                     fontWeight: FontWeight.w400),
                 subtitle1: TextStyle(
-                    color: Color(0xFF555B69),
-                    fontFamily: "Inter",
-                    fontSize: 18),
+                    color: kOffWhite,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400),
                 headline1: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 40,
-                    color: Colors.black,
-                    fontFamily: "Source Serif Pro"))),
+                  fontWeight: FontWeight.w700,
+                  fontSize: 40,
+                  color: kOffWhite,
+                ))),
         home: Scaffold(
-          backgroundColor: Color(0xFFEDEDED),
+          backgroundColor: kOffWhite,
           body: SafeArea(
             child: LoadingScreen(),
           ),

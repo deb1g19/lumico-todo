ff3595c759f2b377da99f3457d11f4f626d7a75a
Daniel Berry <deb1g19@soton.ac.uk>
2020-12-04 12:46:35 +0000

    Changed the value of kOffWhite

--- ui/todo/lib/constants.dart
+++ ui/todo/lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 const String kAPI_URL = "http://192.168.1.43:8000";
-const Color kOffWhite = Color(0xFFEDEDED);
+const Color kOffWhite = Color(0xFFF3F3F3);
 const Color kInkBlack = Color(0xFF3B3E46);

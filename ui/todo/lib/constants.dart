cab92120ff9a3ef56e780d9cf8a9e446dd9785a3
Daniel Berry <deb1g19@soton.ac.uk>
2020-12-04 16:21:16 +0000

    Deleted the hardcoded server url

--- ui/todo/lib/constants.dart
+++ ui/todo/lib/constants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
-const String kAPI_URL = "http://192.168.1.43:8000";
+// Constant values used throughout the app
+
 const Color kOffWhite = Color(0xFFF3F3F3);
 const Color kInkBlack = Color(0xFF3B3E46);

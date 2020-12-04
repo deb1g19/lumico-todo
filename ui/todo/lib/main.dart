abf8768b1808a0d904197484685570fa15200092
Daniel Berry <deb1g19@soton.ac.uk>
2020-12-04 16:21:06 +0000

    Added named routes

--- ui/todo/lib/main.dart
+++ ui/todo/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/constants.dart';
+import 'package:todo/screens/ip_screen.dart';
 import 'package:todo/screens/loading_screen.dart';
 import 'models/tasks_model.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
+// Main handles some project metadata and route creation for navigation
+
 void main() {
+  // The provider for TasksModel is instantiated here to provide access to this TasksModel instance
+  // for widgets lower in the widget tree.
   runApp(ChangeNotifierProvider(
       create: (context) => TasksModel(), child: MyApp()));
 }
@@ -17,30 +22,32 @@ class MyApp extends StatelessWidget {
     //Status bar color is usually determined by scaffold, but scaffold color is not the same as the bg at the top of the screen, so this looks more natural
     FlutterStatusbarcolor.setStatusBarColor(Colors.blue);
     return MaterialApp(
-        title: 'Todo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-            textTheme: TextTheme(
-                bodyText1: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400),
-                subtitle1: TextStyle(
-                    color: kOffWhite,
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400),
-                headline1: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 40,
-                  color: kOffWhite,
-                ))),
-        home: Scaffold(
-          backgroundColor: kOffWhite,
-          body: SafeArea(
-            child: LoadingScreen(),
-          ),
-        ));
+      title: 'Todo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: TextTheme(
+              bodyText1: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400),
+              subtitle1: TextStyle(
+                  color: kOffWhite, fontSize: 15, fontWeight: FontWeight.w400),
+              headline1: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 40,
+                color: kOffWhite,
+              ))),
+      routes: {
+        "/": (context) => Scaffold(
+              backgroundColor: kOffWhite,
+              body: SafeArea(
+                child: IPScreen(),
+              ),
+            ),
+        "/loading_screen": (context) => LoadingScreen()
+      },
+    );
   }
 }
